b7e68bcd417bf3c5d1ebb4e4bac7c3d18359f42a
DeadEggs <musprattn@stu.otc.school.nz>
2026-05-14 15:06:56 +1200

    basic login and sing up -Database & user type

--- # Import Module.py
+++ # Import Module.py
@@ -19,21 +19,68 @@ def reload_page():
     page.grid_remove()
     page.grid(column=0, row=1)
 
-def log_in_page():
-    clear_page()
-    root.title("Home")
-    global page
-    login_button = Button(page, text = "Log in", command=login)
-    login_button.grid(column=0, row=0)
-    signup_button = Button(page, text = "Sign up", command=signup)
-    signup_button.grid(column=0, row=0)
-    reload_page()
+#Login page
+if 1 == 1:
+    def log_in_page():
+        clear_page()
+        root.title("Home")
+        global page
+        login_button = Button(page, text = "Log in", command=login)
+        login_button.grid(column=0, row=0)
+        signup_button = Button(page, text = "Sign up", command=signup)
+        signup_button.grid(column=1, row=0)
+        reload_page()
 
-def login():
-    clear_page()
+    def login():
+        clear_page()
+        #Back Button
+        back_button = Button(page, text = "back", command=lambda:(log_in_page()))
+        back_button.grid(column=0, row=0)
+        
+        #Input user name
+        user_name_txt = Label(page, text="Username:")
+        user_name_txt.grid(column=1, row=2)
+        user_name_input = Entry(page, width=8)
+        user_name_input.grid(column=2,row=2)
+        
+        #Input password
+        password_txt = Label(page, text="Password:")
+        password_txt.grid(column=1, row=4)
+        password_input = Entry(page, width=8)
+        password_input.grid(column=2,row=4)
 
-def signup():
-    clear_page()
+        clear_page()
+
+    def signup():
+        clear_page()
+        #Back Button
+        back_button = Button(page, text = "back", command=lambda:(log_in_page()))
+        back_button.grid(column=0, row=0)
+
+        #Input user name
+        user_name_txt = Label(page, text="Username:")
+        user_name_txt.grid(column=1, row=2)
+        user_name_input = Entry(page, width=8)
+        user_name_input.grid(column=2,row=2)
+
+        #Input user name
+        email_txt = Label(page, text="Email:")
+        email_txt.grid(column=1, row=2)
+        email_input = Entry(page, width=8)
+        email_input.grid(column=2,row=2)
+
+        #Input password
+        password_txt = Label(page, text="Password:")
+        password_txt.grid(column=1, row=6)
+        password_input = Entry(page, width=8)
+        password_input.grid(column=2,row=6)
+
+        #Cheek password
+        password_cheek_txt = Label(page, text="Renter Password:")
+        password_cheek_txt.grid(column=1, row=8)
+        password_cheek_input = Entry(page, width=8)
+        password_cheek_input.grid(column=2,row=8)
+        clear_page()
 
 def home_page():
     clear_page()
